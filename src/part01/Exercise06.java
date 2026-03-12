850f1c9970dc4b6bf194ad7b78435c75908a8cd3
nicms06 <nicolas.mingorance@iesnervion.es.com>
2026-03-12 11:50:00 +0100

    Ejercicio 06 realizado

--- src/part01/Exercise06.java
+++ src/part01/Exercise06.java
@@ -1,7 +1,57 @@
+/**
+ * Repite la actividad anterior de forma que se inserten los nombres manteniendo el orden alfabético.
+ */
+
 package part01;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Exercise06 {
     public static void main(String[] args) {
+        //Creación de un nuevo Scanner
+        Scanner sc = new Scanner(System.in);
 
+        //Creación de un Linked Hash set
+        TreeSet<String> nombres = new TreeSet<>();
+
+        String nombre = " ";
+
+        System.out.println("Introduzca nombres (Fin para finalizar y no se admiten repetidos): ");
+
+        do{
+            nombre = sc.nextLine();
+            if (!nombre.equalsIgnoreCase("fin")){
+                nombres.add(nombre.toLowerCase());
+            }
+        }while (!nombre.equalsIgnoreCase("fin"));
+
+        System.out.println(Arrays.toString(nombres.toArray()));
+
+        //Cerramos el Scanner
+        sc.close();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
