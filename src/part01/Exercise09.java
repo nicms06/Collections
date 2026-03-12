7cfb188fc6d7f0a3f3ac95f84db4452c27c8fff4
nicms06 <nicolas.mingorance@iesnervion.es.com>
2026-03-12 14:35:15 +0100

    Ejercicio 09 Empezado

---
+++ src/part01/Exercise09.java
@@ -0,0 +1,16 @@
+/**
+ * Implementa una aplicación donde ir registrando el número de veces que ha salido cada número del euromillón.
+ * Para ello tendrás que usar dos colecciones, una para los números y otra para las estrellas.
+ * Al usuario se le debe pedir que introduzca primero los 5 números y a continuación las 2 estrellas del último sorteo.
+ * El programa comprobará si ya se encuentra el número correspondiente en la colección, si no está, lo añadirá y le
+ * pondrá el valor 1. Si ya está, incrementará en 1 su valor.
+ * Las colecciones no pueden tener repetidos y deben estar ordenadas.
+ */
+
+package part01;
+
+public class Exercise09 {
+    public static void main(String[] args) {
+
+    }
+}
